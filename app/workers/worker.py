4ff48fc01b900b00ba4bbefa2e112225849c72ef
Himanshu Bisht <96rch4nr4h@privaterelay.appleid.com>
2026-09-22 22:09:51 +0530

    Harden background worker configuration

--- app/workers/worker.py
+++ app/workers/worker.py
@@ -1,12 +1,12 @@
 from arq.connections import RedisSettings
-from arq.worker import Function, Worker
+from arq.worker import Worker
 
 from app.core.config import settings
 from app.workers.tasks import refresh_market_snapshot
 
 
 class WorkerSettings:
-    functions: list[Function] = [Function(refresh_market_snapshot, name="refresh_market_snapshot")]
+    functions = [refresh_market_snapshot]
     redis_settings = RedisSettings.from_dsn(settings.redis_url)
     max_jobs = 10
     job_timeout = 300
